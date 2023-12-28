9a2289bb049fcd08c6ce3e2e8d1c758a10c9276b
Emilio <progame4you@hotmail.com>
2023-12-28 16:29:25 +0100

    favorites basic implementation test

--- app/src/main/java/com/example/thejokerer/pages/favorites/FavoritesPage.kt
+++ app/src/main/java/com/example/thejokerer/pages/favorites/FavoritesPage.kt
@@ -2,15 +2,25 @@ package com.example.thejokerer.pages.favorites
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.thejokerer.viewmodels.JokeViewModel
 
 @Composable
 fun FavoritesPage() {
+
+    val jokeViewModel: JokeViewModel = viewModel(factory = JokeViewModel.Factory)
+
+
     Column (modifier = Modifier.fillMaxWidth(),
     ){
         Text(text = "Favorites page")
+
+
+        jokeViewModel.getFavoriteJokes()
     }
 
 }
